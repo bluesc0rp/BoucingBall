8ca3d050b3ee0044702052bac33bc0e0a2a6d576
Cyrus Team <cyrusteam@cyruslists.com>
2014-01-17 15:53:37 -0500

    Initial Commit

---
+++ BouncingBall/DRAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DRAppDelegate.h
+//  BouncingBall
+//
+//  Created by Cyrus on 1/17/14.
+//  Copyright (c) 2014 Daniel Ramteke. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DRAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BouncingBall/DRMyScene.h
@@ -0,0 +1,13 @@
+//
+//  DRMyScene.h
+//  BouncingBall
+//
+
+//  Copyright (c) 2014 Daniel Ramteke. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface DRMyScene : SKScene
+
+@end
